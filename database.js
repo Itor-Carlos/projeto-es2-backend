0ac66ce62db69421e8908f20b7116da8dff27312
Itor Carlos <itor51carlos@gmail.com>
2025-03-01 18:58:30 -0300

    add database connection

---
+++ database.js
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+import { config } from "dotenv";
+
+config();
+export const database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+  logging: false,
+});
--- server.js
+++ server.js
@@ -3,13 +3,25 @@ import { config } from 'dotenv';
 config();
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { database } from './database.js';
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json())
-const PORT = process.env.DB_PORT || 3306;
+const PORT = process.env.PORT_APP || 3306;
+
+const syncDB = async () => {
+    try {
+        await database.sync({ force: false });
+        console.log("Banco de dados sincronizado.");
+    } 
+    catch (error) {
+        console.error("Erro ao sincronizar DB:", error);
+    }
+};
 
 app.listen(PORT, () => {
+    syncDB();
     console.log("Servidor iniciaalizado na em http://localhost:" + PORT);
 });
