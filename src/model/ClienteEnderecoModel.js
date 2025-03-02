febe368a0e3aa96770c5550b83e4877fc5e78cb9
Itor Carlos <itor51carlos@gmail.com>
2025-03-02 16:34:32 -0300

    add ClienteEnderecoModel

---
+++ src/model/ClienteEnderecoModel.js
@@ -0,0 +1,22 @@
+import { DataTypes } from "sequelize";
+import { database } from "../../database.js";
+import { config } from "dotenv";
+
+config();
+
+export const ClienteEndereco = database.define("ClienteEndereco", {
+  idcliente: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  idendereco: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  }
+}, {
+  tableName: "cliente_endereco",
+  schema: process.env.DB_SCHEMA,
+  timestamps: false
+});
