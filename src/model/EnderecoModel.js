be1b9b5a724e845d6bd95d057ed33a334395a324
Itor Carlos <itor51carlos@gmail.com>
2025-03-02 16:26:56 -0300

    add model Endereco

---
+++ src/model/EnderecoModel.js
@@ -0,0 +1,40 @@
+import { DataTypes } from "sequelize";
+import { database } from "../../database.js";
+import { config } from "dotenv";
+
+config()
+export const Endereco = database.define("Endereco", {
+  idendereco: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  estado: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cidade: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  bairro: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  logradouro: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }, 
+  numero: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  cep: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: "endereco",
+  schema: process.env.DB_SCHEMA,
+  timestamps: false
+});
