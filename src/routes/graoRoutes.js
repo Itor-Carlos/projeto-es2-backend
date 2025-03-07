2da23a58eba066d1b6a83c8d1860de9f5100fd81
Itor Carlos <itor51carlos@gmail.com>
2025-03-07 19:11:14 -0300

    fix nome do controller

--- src/routes/graoRoutes.js
+++ src/routes/graoRoutes.js
@@ -1,11 +1,11 @@
 import express, {Router} from 'express';
-import Grao from '../controller/GraoController.js';
+import GraoController from '../controller/GraoController.js';
 
 export const routerGrao = Router();
 
-routerGrao.post('/', Grao.create);
-routerGrao.get('/', Grao.findAll);
-routerGrao.delete('/:id', Grao.delete);
-routerGrao.put('/:id', Grao.update);
+routerGrao.post('/', GraoController.create);
+routerGrao.get('/', GraoController.findAll);
+routerGrao.delete('/:id', GraoController.delete);
+routerGrao.put('/:id', GraoController.update);
 
 export default routerGrao;
