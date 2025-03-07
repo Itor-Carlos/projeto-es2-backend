6df5a0dc08ffc19b713918b693a01ceb1976e081
Itor Carlos <itor51carlos@gmail.com>
2025-03-07 12:08:37 -0300

    add model Area

---
+++ src/model/AreaModel.js
@@ -0,0 +1,25 @@
+import { DataTypes } from "sequelize";
+import { database } from "../../database.js";
+import { config } from "dotenv";
+
+config()
+export const Area = database.define("Area", {
+  idarea: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  hectares: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  emuso: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+}, {
+  tableName: "area",
+  schema: process.env.DB_SCHEMA,
+  timestamps: false
+});
