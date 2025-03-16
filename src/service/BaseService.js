7ddd75279b6492bfe0b7861a7475e1d27b1f751d
Romulo Santana <romulomsantana@hotmail.com>
2025-03-16 17:27:55 -0300

    refactor: modulariza codigo dos Services

---
+++ src/service/BaseService.js
@@ -0,0 +1,62 @@
+export class BaseService {
+  constructor(Model, idField, options = {}) {
+      this.Model = Model;
+      this.idField = idField;
+      this.validateEmailAndDoc = options.validateEmailAndDoc || false;
+  }
+
+  async findById(id) {
+      return await this.Model.findByPk(id);
+  }
+
+  async findAll(page = 1, pageSize = 10) {
+      return await this.Model.findAndCountAll({
+          limit: pageSize,
+          offset: (page - 1) * pageSize
+      });
+  }
+
+  async delete(id) {
+      if(!id) {
+          throw new Error("Id não informado.");
+      }
+      return await this.Model.destroy({
+          where: { [this.idField]: id }
+      });
+  }
+
+  async create(data) {
+      validateModel(this.Model, data);
+
+      if (this.validateEmailAndDoc) {
+          const validEmail = validateEmail(data.email);
+          if(!validEmail) {
+              throw new Error("Email inválido.");
+          }
+
+          const validDocumento = validateDocumento(data.documento);
+          if(!validDocumento) {
+              throw new Error("Documento inválido.");
+          }
+
+          const emailAlredyUsed = await this.Model.findByEmail(data.email);
+          if(emailAlredyUsed) {
+              throw new Error("Email já cadastrado.");
+          }  
+
+          const documentoAlredyUsed = await this.Model.findByDocumento(data.documento);
+          if(documentoAlredyUsed) {
+              throw new Error("Documento já cadastrado.");
+          }
+      }
+
+      return await this.Model.create(data);
+  }
+
+  async update(id, data) {
+      validateModel(this.Model, {id, ...data});
+      return await this.Model.update(data, { 
+          where: { [this.idField]: id } 
+      });
+  }
+}
--- src/service/ClienteService.js
+++ src/service/ClienteService.js
@@ -1,47 +1,10 @@
 import { Cliente } from "../model/ClienteModel.js";
-import { validateModel } from "../utils/validation.js";
-import { validateDocumento, validateEmail } from "../utils/validation.js";
+import { BaseService } from "./BaseService.js";
 
-class ClienteService {
-    async findAll(page, pageSize) {
-        return await Cliente.findAndCountAll({
-            limit: pageSize,
-            offset: (page - 1) * pageSize
-        });
-    }
 
-    async delete(id) {
-        if(!id){
-            throw new Error("Id não informado.");
-        }
-        return await Cliente.destroy({where: {idcliente: id}});
-    }
-
-    async create(cliente){
-        validateModel(Cliente, cliente);
-    
-        const validEmail = validateEmail(cliente.email);
-        if(!validEmail){
-            throw new Error("Email inválido.");
-        }
-
-        const validDocumento = validateDocumento(cliente.documento);
-        if(!validDocumento){
-            throw new Error("Documento inválido.");
-        }
-
-        const emailAlredyUsed = await Cliente.findByEmail(cliente.email);
-        if(emailAlredyUsed){
-            throw new Error("Email já cadastrado.");
-        }  
-
-        const documentoAlredyUsed = await Cliente.findByDocumento(cliente.documento);
-        if(documentoAlredyUsed){
-            throw new Error("Documento já cadastrado.");
-        }
-
-        const createdCliente = await Cliente.create(cliente);
-        return createdCliente;
+class ClienteService extends BaseService {
+    constructor() {
+        super(Cliente, 'idcliente', { validateEmailAndDoc: true });
     }
 }
 
