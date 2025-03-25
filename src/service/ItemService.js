2d38a05c6ba21ba6445e8d204ba166d674793507
Itor Carlos <itor51carlos@gmail.com>
2025-03-24 22:32:42 -0300

    add create and update de cadastro de pedidos

--- src/service/ItemService.js
+++ src/service/ItemService.js
@@ -5,6 +5,10 @@ class ItemService extends BaseService {
     constructor() {
         super(Item, 'iditem', { validateEmailAndDoc: false, includes: ["pedido", "produto"] });
     }
+
+    findByIdPedido = async (idPedido) => {
+        return await Item.findAll({ where: { idpedido: idPedido } });
+    }
 }
 
 export default new ItemService();
